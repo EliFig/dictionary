beb773fd3c7bd9e508715e45f4c7c17517fc40f8
Elifigue <elianafigueroa47@gmail.com>
2022-09-30 12:34:35 +0200

    rendered the words meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,29 +1,52 @@
 import React, { useState } from "react";
+import Result from "./Result.js"
 import axios from "axios";
 
-export default function Dictionary(){
-    let[keyWord, setkeyWord] = useState("");
+export default function Dictionary(props){
+    let[keyWord, setkeyWord] = useState("props.defaultKeyword");
+    let [loaded, setLoaded] = useState(false);
+    let[definition, setDefinition] = useState(null)
 
     function handleResponse(response){
-        console.log(response.data[0]);
+        setDefinition(response.data[0]);
     }
-      function Search(event){
-event.preventDefault();
-let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
-axios.get({apiUrl}).then(handleResponse);
 
-alert(`Searching for ${keyWord}`);
-        }
+      function load() {
+    setLoaded(true);
+    Search();
+  }
+
+
+  function Search() {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyWord}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    Search();
+  }
 
         function HandleKeyWord(event){
 setkeyWord(event.target.value);
         }
+        if(loaded){
     return(
-    <form onSubmit={Search}>
-            <input onChange={HandleKeyWord} type="Search" placeholder="Type a word" outoFocus={true}/>
-            <button onChange={HandleKeyWord} type="button" class="btn btn-primary">Primary</button>
+        <div className="Dictionary">
+            <section>
+    <form onSubmit={handleSubmit}>
+            <input onChange={HandleKeyWord} type="Search" placeholder="Type a word"/>
+            <button onChange={HandleKeyWord} type="button" className="btn btn-primary">Primary</button>
     
         </form>
-    )
+        </section>
+        <Result definition={definition}/>
+        </div>
+    );
+        }else{
+            load();
+            return "Loading!"
+        }
     
 }
